45ac3d483fff3933f751dce9897bd50d0cceabe3
Burca <nicolae-razvan.burca@capgemini.com>
2021-08-02 18:21:34 +0300

    fixed searchbar for all parameters

--- client-manager/src/Components/SearchClient2.js
+++ client-manager/src/Components/SearchClient2.js
@@ -1,29 +1,30 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./Search.css";
 import Scroll from "./Scroll";
-import SearchCard from "./Card";
+import SearchCard from "./SearchCard";
 
 function Search({ clients, isLoading }) {
   const [searchField, setSearchField] = useState("");
 
-  function filteredPersons() {
-    return clients.filter((person) => {
-      return (
-        person.first_name.toLowerCase().includes(searchField.toLowerCase()) ||
-        person.last_name.toLowerCase().includes(searchField.toLowerCase()) ||
-        person.phone.toLowerCase().includes(searchField.toLowerCase()) ||
-        person.city.toLowerCase().includes(searchField.toLowerCase()) ||
-        person.state.toLowerCase().includes(searchField.toLowerCase()) ||
-        person.car_number.toLowerCase().includes(searchField.toLowerCase()) ||
-        person.cnp.toLowerCase().includes(searchField.toLowerCase()) ||
-        person.street.toLowerCase().includes(searchField.toLowerCase()) ||
-        person.street_number
-          .toString()
-          .toLowerCase()
-          .includes(searchField.toLowerCase())
-      );
-    });
-  }
+  const filteredClients = clients.filter((person) => {
+    return (
+      (person.first_name &&
+        person.first_name.toLowerCase().includes(searchField.toLowerCase())) ||
+      (person.last_name &&
+        person.last_name.toLowerCase().includes(searchField.toLowerCase())) ||
+      (person.car_number &&
+        person.car_number.toLowerCase().includes(searchField.toLowerCase())) ||
+      (person.street &&
+        person.street.toLowerCase().includes(searchField.toLowerCase())) ||
+      (person.phone && person.phone.includes(searchField)) ||
+      (person.state &&
+        person.state.toLowerCase().includes(searchField.toLowerCase())) ||
+      (person.city &&
+        person.city.toLowerCase().includes(searchField.toLowerCase())) ||
+      (person.cnp && person.cnp.includes(searchField)) ||
+      (person.street_number && person.street_number.includes(searchField))
+    );
+  });
 
   const handleChange = (e) => {
     setSearchField(e.target.value);
@@ -33,13 +34,13 @@ function Search({ clients, isLoading }) {
     if (!clients) return <h3>Loading data ...</h3>;
 
     if (searchField === "") return <h2>No search criterias currently</h2>;
-    else if (filteredPersons().length === 0)
+    else if (filteredClients.length === 0)
       return <h3>No results found for your search</h3>;
     else
       return (
         <Scroll>
           <ul>
-            {filteredPersons().map((client) => (
+            {filteredClients.map((client) => (
               <SearchCard client={client} />
             ))}
           </ul>
