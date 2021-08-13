ecd8881c46f0fa0f341eda2adb1ec3ee30239aea
Burca <nicolae-razvan.burca@capgemini.com>
2021-08-13 11:55:36 +0300

    added particle animation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Navigation from './Components/Navigation';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navigation from "./Components/Navigation";
+import Particles from "react-particles-js";
 
-
-
-
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 40,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+  },
+};
 
 function App() {
   return (
     <div className="App">
+      <Particles className="particles" params={particlesOptions} />
       <Navigation />
-
     </div>
   );
 }
