cd9f057fe9b0524b2f55376b4d4f9548c38bc1e6
Burca <nicolae-razvan.burca@capgemini.com>
2021-08-13 10:52:57 +0300

    styling add clients

--- src/Components/FormSignup.js
+++ src/Components/FormSignup.js
@@ -83,243 +83,258 @@ const FormSignup = ({ submitForm }) => {
   return (
     <div className="form-content-right">
       <form onSubmit={handleSubmit} className="form" noValidate>
-        <h1>Fill the following form to add a new client </h1>
+        <h1>Add New Client </h1>
         <LinearWithValueLabel progress={progress < 99 ? progress : 100} />
-        <div className="form-inputs">
-          <label className="form-label">First Name</label>
-          <input
-            id="First-Name"
-            type="text"
-            name="FirstName"
-            className="form-input"
-            value={values.FirstName}
-            onChange={handleChange}
-            onBlur={(e) => {
-              if (e.target.value && !visited[0]) {
-                setProgress(progress + 11.11);
-                visited[0] = true;
-              } else if (visited[0] && e.target.value === "") {
-                setProgress(progress - 11.11);
-                visited[0] = false;
-              }
-              setRTErrors({
-                ...rtErrors,
-                firstName: v.CheckFirstName(firstName),
-              });
-            }}
-          />
-          {rtErrors && rtErrors.firstName && <p>{rtErrors.firstName}</p>}
-        </div>
-        <div className="form-inputs">
-          <label className="form-label">Last Name</label>
-          <input
-            id="Last-Name"
-            type="text"
-            name="LastName"
-            className="form-input"
-            value={values.LastName}
-            onChange={handleChange}
-            onBlur={(e) => {
-              if (e.target.value && !visited[1]) {
-                setProgress(progress + 11.11);
-                visited[1] = true;
-              } else if (visited[1] && e.target.value === "") {
-                setProgress(progress - 11.11);
-                visited[1] = false;
-              }
-              setRTErrors({
-                ...rtErrors,
-                lastName: v.CheckLastName(lastName),
-              });
-            }}
-          />
-          {rtErrors && rtErrors.lastName && <p>{rtErrors.lastName}</p>}
-        </div>
-        <div className="form-inputs">
-          <label className="form-label">Street Name</label>
-          <input
-            id="Street-Name"
-            type="text"
-            name="StreetName"
-            className="form-input"
-            value={values.StreetName}
-            onChange={handleChange}
-            onBlur={(e) => {
-              if (e.target.value && !visited[2]) {
-                setProgress(progress + 11.11);
-                visited[2] = true;
-              } else if (visited[2] && e.target.value === "") {
-                setProgress(progress - 11.11);
-                visited[2] = false;
-              }
-              setRTErrors({
-                ...rtErrors,
-                streetName: v.CheckStreetName(streetName),
-              });
-            }}
-          />
-          {rtErrors && rtErrors.streetName && <p>{rtErrors.streetName}</p>}
-        </div>
+        <fieldset>
+          <div className="form-inputs">
+            <label className="form-label">First Name</label>
+            <input
+              id="First-Name"
+              type="text"
+              name="FirstName"
+              className="form-input"
+              value={values.FirstName}
+              placeholder="Enter your first name..."
+              onChange={handleChange}
+              onBlur={(e) => {
+                if (e.target.value && !visited[0]) {
+                  setProgress(progress + 11.11);
+                  visited[0] = true;
+                } else if (visited[0] && e.target.value === "") {
+                  setProgress(progress - 11.11);
+                  visited[0] = false;
+                }
+                setRTErrors({
+                  ...rtErrors,
+                  firstName: v.CheckFirstName(firstName),
+                });
+              }}
+            />
+            {rtErrors && rtErrors.firstName && <p>{rtErrors.firstName}</p>}
+          </div>
+          <div className="form-inputs">
+            <label className="form-label">Last Name</label>
+            <input
+              id="Last-Name"
+              type="text"
+              name="LastName"
+              className="form-input"
+              value={values.LastName}
+              placeholder="Enter your last name..."
+              onChange={handleChange}
+              onBlur={(e) => {
+                if (e.target.value && !visited[1]) {
+                  setProgress(progress + 11.11);
+                  visited[1] = true;
+                } else if (visited[1] && e.target.value === "") {
+                  setProgress(progress - 11.11);
+                  visited[1] = false;
+                }
+                setRTErrors({
+                  ...rtErrors,
+                  lastName: v.CheckLastName(lastName),
+                });
+              }}
+            />
+            {rtErrors && rtErrors.lastName && <p>{rtErrors.lastName}</p>}
+          </div>
+          <div className="form-inputs">
+            <label className="form-label">Street Name</label>
+            <input
+              id="Street-Name"
+              type="text"
+              name="StreetName"
+              className="form-input"
+              value={values.StreetName}
+              placeholder="Enter your street name..."
+              onChange={handleChange}
+              onBlur={(e) => {
+                if (e.target.value && !visited[2]) {
+                  setProgress(progress + 11.11);
+                  visited[2] = true;
+                } else if (visited[2] && e.target.value === "") {
+                  setProgress(progress - 11.11);
+                  visited[2] = false;
+                }
+                setRTErrors({
+                  ...rtErrors,
+                  streetName: v.CheckStreetName(streetName),
+                });
+              }}
+            />
+            {rtErrors && rtErrors.streetName && <p>{rtErrors.streetName}</p>}
+          </div>
 
-        <div className="form-inputs">
-          <label className="form-label">Street Number</label>
-          <input
-            id="Street-No"
-            type="text"
-            name="StreetNo"
-            className="form-input"
-            value={values.StreetNo}
-            onChange={handleChange}
-            onBlur={(e) => {
-              if (e.target.value && !visited[3]) {
-                setProgress(progress + 11.11);
-                visited[3] = true;
-              } else if (visited[3] && e.target.value === "") {
-                setProgress(progress - 11.11);
-                visited[3] = false;
-              }
-              setRTErrors({
-                ...rtErrors,
-                streetNumber: v.CheckStreetNumber(streetNumber),
-              });
-            }}
-          />
-          {rtErrors && rtErrors.streetNumber && <p>{rtErrors.streetNumber}</p>}
-        </div>
+          <div className="form-inputs">
+            <label className="form-label">Street Number</label>
+            <input
+              id="Street-No"
+              type="text"
+              name="StreetNo"
+              className="form-input"
+              value={values.StreetNo}
+              placeholder="Enter your street number..."
+              onChange={handleChange}
+              onBlur={(e) => {
+                if (e.target.value && !visited[3]) {
+                  setProgress(progress + 11.11);
+                  visited[3] = true;
+                } else if (visited[3] && e.target.value === "") {
+                  setProgress(progress - 11.11);
+                  visited[3] = false;
+                }
+                setRTErrors({
+                  ...rtErrors,
+                  streetNumber: v.CheckStreetNumber(streetNumber),
+                });
+              }}
+            />
+            {rtErrors && rtErrors.streetNumber && (
+              <p>{rtErrors.streetNumber}</p>
+            )}
+          </div>
 
-        <div className="form-inputs">
-          <label className="form-label">City</label>
-          <input
-            id="city"
-            type="text"
-            name="city"
-            className="form-input"
-            value={values.city}
-            onChange={handleChange}
-            onBlur={(e) => {
-              if (e.target.value && !visited[4]) {
-                setProgress(progress + 11.11);
-                visited[4] = true;
-              } else if (visited[4] && e.target.value === "") {
-                setProgress(progress - 11.11);
-                visited[4] = false;
-              }
-              setRTErrors({
-                ...rtErrors,
-                city: v.CheckCity(city),
-              });
-            }}
-          />
-          {rtErrors && rtErrors.city && <p>{rtErrors.city}</p>}
-        </div>
+          <div className="form-inputs">
+            <label className="form-label">City</label>
+            <input
+              id="city"
+              type="text"
+              name="city"
+              className="form-input"
+              value={values.city}
+              placeholder="Enter your city..."
+              onChange={handleChange}
+              onBlur={(e) => {
+                if (e.target.value && !visited[4]) {
+                  setProgress(progress + 11.11);
+                  visited[4] = true;
+                } else if (visited[4] && e.target.value === "") {
+                  setProgress(progress - 11.11);
+                  visited[4] = false;
+                }
+                setRTErrors({
+                  ...rtErrors,
+                  city: v.CheckCity(city),
+                });
+              }}
+            />
+            {rtErrors && rtErrors.city && <p>{rtErrors.city}</p>}
+          </div>
 
-        <div className="form-inputs">
-          <label className="form-label">State</label>
-          <input
-            id="state"
-            type="text"
-            name="state"
-            className="form-input"
-            value={values.state}
-            onChange={handleChange}
-            onBlur={(e) => {
-              if (e.target.value && !visited[5]) {
-                setProgress(progress + 11.11);
-                visited[5] = true;
-              } else if (visited[5] && e.target.value === "") {
-                setProgress(progress - 11.11);
-                visited[5] = false;
-              }
-              setRTErrors({
-                ...rtErrors,
-                state: v.CheckState(state),
-              });
-            }}
-          />
-          {rtErrors && rtErrors.state && <p>{rtErrors.state}</p>}
-        </div>
+          <div className="form-inputs">
+            <label className="form-label">State</label>
+            <input
+              id="state"
+              type="text"
+              name="state"
+              className="form-input"
+              value={values.state}
+              placeholder="Enter your state..."
+              onChange={handleChange}
+              onBlur={(e) => {
+                if (e.target.value && !visited[5]) {
+                  setProgress(progress + 11.11);
+                  visited[5] = true;
+                } else if (visited[5] && e.target.value === "") {
+                  setProgress(progress - 11.11);
+                  visited[5] = false;
+                }
+                setRTErrors({
+                  ...rtErrors,
+                  state: v.CheckState(state),
+                });
+              }}
+            />
+            {rtErrors && rtErrors.state && <p>{rtErrors.state}</p>}
+          </div>
 
-        <div className="form-inputs">
-          <label className="form-label">Phone Number</label>
-          <input
-            id="Phone-Number"
-            type="text"
-            name="PhoneNumber"
-            className="form-input"
-            value={values.PhoneNumber}
-            onChange={handleChange}
-            onBlur={(e) => {
-              if (e.target.value && !visited[6]) {
-                setProgress(progress + 11.11);
-                visited[6] = true;
-              } else if (visited[6] && e.target.value === "") {
-                setProgress(progress - 11.11);
-                visited[6] = false;
-              }
-              setRTErrors({
-                ...rtErrors,
-                phone: v.CheckPhone(phone),
-              });
-            }}
-          />
-          {rtErrors && rtErrors.phone && <p>{rtErrors.phone}</p>}
-        </div>
+          <div className="form-inputs">
+            <label className="form-label">Phone Number</label>
+            <input
+              id="Phone-Number"
+              type="text"
+              name="PhoneNumber"
+              className="form-input"
+              value={values.PhoneNumber}
+              placeholder="Enter your phone number..."
+              onChange={handleChange}
+              onBlur={(e) => {
+                if (e.target.value && !visited[6]) {
+                  setProgress(progress + 11.11);
+                  visited[6] = true;
+                } else if (visited[6] && e.target.value === "") {
+                  setProgress(progress - 11.11);
+                  visited[6] = false;
+                }
+                setRTErrors({
+                  ...rtErrors,
+                  phone: v.CheckPhone(phone),
+                });
+              }}
+            />
+            {rtErrors && rtErrors.phone && <p>{rtErrors.phone}</p>}
+          </div>
 
-        <div className="form-inputs">
-          <label className="form-label"> Social Security Number </label>
-          <input
-            id="Social Security Number"
-            type="text"
-            name="SocialSecurityNumber"
-            className="form-input"
-            value={values.SocialSecurityNumber}
-            onChange={handleChange}
-            onBlur={(e) => {
-              if (e.target.value && !visited[7]) {
-                setProgress(progress + 11.11);
-                visited[7] = true;
-              } else if (visited[7] && e.target.value === "") {
-                setProgress(progress - 11.11);
-                visited[7] = false;
-              }
-              setRTErrors({
-                ...rtErrors,
-                ssn: v.CheckSSN(ssn),
-              });
-            }}
-          />
-          {rtErrors && rtErrors.ssn && <p>{rtErrors.ssn}</p>}
-        </div>
+          <div className="form-inputs">
+            <label className="form-label"> Social Security Number </label>
+            <input
+              id="Social Security Number"
+              type="text"
+              name="SocialSecurityNumber"
+              className="form-input"
+              value={values.SocialSecurityNumber}
+              placeholder="Enter your ssn..."
+              onChange={handleChange}
+              onBlur={(e) => {
+                if (e.target.value && !visited[7]) {
+                  setProgress(progress + 11.11);
+                  visited[7] = true;
+                } else if (visited[7] && e.target.value === "") {
+                  setProgress(progress - 11.11);
+                  visited[7] = false;
+                }
+                setRTErrors({
+                  ...rtErrors,
+                  ssn: v.CheckSSN(ssn),
+                });
+              }}
+            />
+            {rtErrors && rtErrors.ssn && <p>{rtErrors.ssn}</p>}
+          </div>
 
-        <div className="form-inputs">
-          <label className="form-label">License Plate </label>
-          <input
-            id="Licence-plate"
-            type="text"
-            name="Licenceplate"
-            className="form-input"
-            value={values.Licenceplate}
-            onChange={handleChange}
-            onBlur={(e) => {
-              if (e.target.value && !visited[8]) {
-                setProgress(progress + 11.11);
-                visited[8] = true;
-              } else if (visited[8] && e.target.value === "") {
-                setProgress(progress - 11.11);
-                visited[8] = false;
-              }
-              setRTErrors({
-                ...rtErrors,
-                licensePlate: v.CheckLicensePlate(licensePlate),
-              });
-            }}
-          />
-          {rtErrors && rtErrors.licensePlate && <p>{rtErrors.licensePlate}</p>}
-        </div>
+          <div className="form-inputs">
+            <label className="form-label">License Plate </label>
+            <input
+              id="Licence-plate"
+              type="text"
+              name="Licenceplate"
+              className="form-input"
+              value={values.Licenceplate}
+              placeholder="Enter your licence plate..."
+              onChange={handleChange}
+              onBlur={(e) => {
+                if (e.target.value && !visited[8]) {
+                  setProgress(progress + 11.11);
+                  visited[8] = true;
+                } else if (visited[8] && e.target.value === "") {
+                  setProgress(progress - 11.11);
+                  visited[8] = false;
+                }
+                setRTErrors({
+                  ...rtErrors,
+                  licensePlate: v.CheckLicensePlate(licensePlate),
+                });
+              }}
+            />
+            {rtErrors && rtErrors.licensePlate && (
+              <p>{rtErrors.licensePlate}</p>
+            )}
+          </div>
 
-        <button className="form-input-btn" type="submit">
-          submit
-        </button>
+          <button className="form-input-btn" type="submit">
+            submit
+          </button>
+        </fieldset>
         <span className="form-input-login"></span>
       </form>
     </div>
